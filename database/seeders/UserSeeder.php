fd2ba66380cf86324d6e21de37d9af6ea9b27cf5
Iriogbe Peter <iriogbepeter22@gmail.com>
2024-06-26 14:14:20 +0100

    .

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -10,21 +10,31 @@ use Illuminate\Support\Str;
 
 class UserSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        User::insert([
-          [
-         'name' => 'Admin Sudo',
-          'email' => 'admin@gmail.com',
-          'role' => 'Admin',
-          'avatar' => null,
-          'email_verified_at' => now(),
-          'password' => Hash::make('password'),
-          'remember_token' => Str::random(10),
-        ], 
-      ]);
-    }
+  /**
+   * Run the database seeds.
+   */
+  public function run(): void
+  {
+    User::insert([
+      [
+        'name' => 'Admin Sudo',
+        'email' => 'admin@gmail.com',
+        'role' => 'Admin',
+        'avatar' => null,
+        'email_verified_at' => now(),
+        'password' => Hash::make('password'),
+        'remember_token' => Str::random(10),
+      ],
+
+      [
+        'name' => 'Peter Iriogbe',
+        'email' => 'iriogbepeter22@gmail.com',
+        'role' => 'Moderator',
+        'avatar' => null,
+        'email_verified_at' => now(),
+        'password' => Hash::make('password'),
+        'remember_token' => Str::random(10),
+      ],
+    ]);
+  }
 }
